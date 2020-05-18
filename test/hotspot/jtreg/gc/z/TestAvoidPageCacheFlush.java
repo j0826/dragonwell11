bbbe358e3f368275160e738eff63aaacbc43360d
Hao Tang <albert.th@alibaba-inc.com>
2020-05-18 17:01:06 +0800

    [GC] gc/z/TestAvoidPageCacheFlush.java fails with unexpected allocation stall
    Summary: Use single-threaded mutator for gc/z/TestAvoidPageCacheFlush.java
    
    Test Plan: gc/z/TestAvoidPageCacheFlush.java
    
    Reviewers: sanhong.lsh, chuansheng.lcs, maoliang.ml, tongbao.ztb
    
    Issue: https://aone.alibaba-inc.com/task/27802088
    
    CR: https://code.aone.alibaba-inc.com/xcode/jdk11/codereview/2995851

--- test/hotspot/jtreg/gc/z/TestAvoidPageCacheFlush.java
+++ test/hotspot/jtreg/gc/z/TestAvoidPageCacheFlush.java
@@ -22,116 +22,106 @@ public class TestAvoidPageCacheFlush {
             return; // ignore slow debug build because allocation is too fast for garbage collection
         }
 
+        int heapSizeGB = 4; // 4GB
+
         ProcessTools.executeTestJvm(new String[]{"-XX:+UnlockExperimentalVMOptions",
                                                  "-XX:+UseZGC",
                                                  "-XX:+UnlockDiagnosticVMOptions",
-                                                 "-Xms4g",
-                                                 "-Xmx4g",
+                                                 "-Xms" + heapSizeGB + "g",
+                                                 "-Xmx" + heapSizeGB + "g",
                                                  "-XX:ParallelGCThreads=2",
                                                  "-XX:ConcGCThreads=4",
                                                  "-XX:+ZBalancePageCache", // page cache flush if this line is removed
                                                  "-Xlog:gc,gc+heap",
-                                                 AvoidPageCacheFlush.class.getName()})
+                                                 AvoidPageCacheFlush.class.getName(),
+                                                 Integer.toString(heapSizeGB)})
                                                  .shouldNotContain("Allocation Stall")
                                                  .shouldNotContain("Page Cache Flushed")
                                                  .shouldHaveExitValue(0);
     }
 
     static class AvoidPageCacheFlush {
-        private static final int THREADS_NUM = 12;
-
-        public static void main(String[] args) {
-            Thread[] w = new Thread[THREADS_NUM];
-            for (int i = 0; i < THREADS_NUM; i++) {
-                w[i] = new AllocationThread();
-                w[i].start();
-            }
-            for (int i = 0; i < THREADS_NUM; i++) {
-                try {
-                    w[i].join();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-    }
-
-    static class AllocationThread extends Thread {
-        SmallContainer smallContainer;
-        MediumContainer mediumContainer;
-
-        public AllocationThread() {
-            this.smallContainer = new SmallContainer();
-            this.mediumContainer = new MediumContainer();
-        }
-
         /*
          * small object: fast allocation
-         * medium object: slow allocation, fast accessing, abrupt deletion
+         * medium object: slow allocation, periodic deletion
          */
-        public void run() {
-            int i = 0, j = 0, addedMediumObjectCount = 0;
-            while (true) {
-                smallContainer.createAndSaveObject();
-                mediumContainer.accessObject(10); // frequently accessing medium object
-                i++;
-                if (i == 150) {
-                    LockSupport.parkNanos(10); // make allocation slower
-                    i = 0;
-                }
-                j++;
-                if (j == 500000) {
+        public static void main(String[] args) throws Exception {
+            long heapSizeKB = Long.valueOf(args[0]) << 20;
+
+            SmallContainer smallContainer = new SmallContainer((long)(heapSizeKB * 0.4));     // 40% heap for live small objects
+            MediumContainer mediumContainer = new MediumContainer((long)(heapSizeKB * 0.3));  // 30% heap for live medium objects
+
+            int totalSmall = smallContainer.getTotalObjects();
+            int totalMedium = mediumContainer.getTotalObjects();
+
+            int addedSmall = 0;
+            int addedMedium = 1; // should not be divided by zero
+
+            while (addedMedium < totalMedium * 10) {
+                if (totalSmall / totalMedium > addedSmall / addedMedium) { // keep the ratio of allocated small/medium objects
+                    smallContainer.createAndSaveObject();
+                    addedSmall ++;
+                } else {
                     mediumContainer.createAndAppendObject();
-                    j = 0;
-                    addedMediumObjectCount++;
-                    if (addedMediumObjectCount > 1.5 * MediumContainer.MEDIUM_OBJ_LIMIT) {
-                        break;
-                    }
+                    addedMedium ++;
+                }
+                if ((addedSmall + addedMedium) % 50 == 0) {
+                    LockSupport.parkNanos(500); // make allocation slower
                 }
             }
         }
     }
 
     static class SmallContainer {
-        private final static int SMALL_OBJ_LIMIT = 200000;
+        private final int KB_PER_OBJECT = 64; // 64KB per object
         private final Random RANDOM = new Random();
 
-        private byte[][] smallObjectArray = new byte[SMALL_OBJ_LIMIT][];
+        private byte[][] smallObjectArray;
+        private long totalKB;
+        private int totalObjects;
+
+        SmallContainer(long totalKB) {
+            this.totalKB = totalKB;
+            totalObjects = (int)(totalKB / KB_PER_OBJECT);
+            smallObjectArray = new byte[totalObjects][];
+        }
+
+        int getTotalObjects() {
+            return totalObjects;
+        }
 
         // random insertion (with random deletion)
         void createAndSaveObject() {
-            smallObjectArray[RANDOM.nextInt(SMALL_OBJ_LIMIT)] = new byte[200];
+            smallObjectArray[RANDOM.nextInt(totalObjects)] = new byte[KB_PER_OBJECT << 10];
         }
     }
 
     static class MediumContainer {
-        public final static int MEDIUM_OBJ_LIMIT = 30;
-        private final static Random RANDOM = new Random();
+        private final int KB_PER_OBJECT = 512; // 512KB per object
 
-        private byte[][] mediumObjectArray = new byte[MEDIUM_OBJ_LIMIT][];
+        private byte[][] mediumObjectArray;
         private int mediumObjectArrayCurrentIndex = 0;
+        private long totalKB;
+        private int totalObjects;
+
+        MediumContainer(long totalKB) {
+            this.totalKB = totalKB;
+            totalObjects = (int)(totalKB / KB_PER_OBJECT);
+            mediumObjectArray = new byte[totalObjects][];
+        }
+
+        int getTotalObjects() {
+            return totalObjects;
+        }
 
         void createAndAppendObject() {
-            if (mediumObjectArrayCurrentIndex == MEDIUM_OBJ_LIMIT) {
-                dropAnArray(); // delete a lot of medium objects in an operations
+            if (mediumObjectArrayCurrentIndex == totalObjects) { // periodic deletion
+                mediumObjectArray = new byte[totalObjects][]; // also delete all medium objects in the old array
                 mediumObjectArrayCurrentIndex = 0;
             } else {
-                mediumObjectArray[mediumObjectArrayCurrentIndex] = new byte[2 << 20]; // 2 MB
+                mediumObjectArray[mediumObjectArrayCurrentIndex] = new byte[KB_PER_OBJECT << 10];
                 mediumObjectArrayCurrentIndex ++;
             }
-        }
-
-        byte[] accessObject(long x) {
-            if (mediumObjectArrayCurrentIndex == 0) {
-                return null;
-            }
-            return mediumObjectArray[RANDOM.nextInt(mediumObjectArrayCurrentIndex)];
-        }
-
-        private void dropAnArray() {
-            // in the real environment, the old mediumObjectArray may be dumped to the disk
-            mediumObjectArray = new byte[MEDIUM_OBJ_LIMIT][]; // also delete all medium objects in the old array
-            mediumObjectArrayCurrentIndex = 0;
         }
     }
 }
