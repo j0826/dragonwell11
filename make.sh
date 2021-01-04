9cfc5efcfa777b1c1dd41661cd710d2bfb65b247
joeylee.lz <joeylee.lz@alibaba-inc.com>
2021-01-04 15:04:08 +0800

    t

--- make.sh
+++ make.sh
@@ -1,14 +1,2 @@
-RET=0
-for p in ${EXPECTED_PATTERN[*]}
-do
-    cat /tmp/systemProperty.out | grep "$p"
-    if [ 0 != $? ]; then RET=1; fi
-done
-
-\rm -f /tmp/systemProperty*
-
-ldd ${NEW_JAVA_HOME}/lib/libzip.so | grep libz
-if [ 0 != $? ]; then RET=1; fi
-echo "================= Sanity test end ======================"
 
 exit ${RET}
